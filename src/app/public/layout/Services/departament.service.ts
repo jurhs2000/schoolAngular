908831ebc0a0d2d8b9fbafb3c917cf578fea9dec
Carlos Rodriguez <carlosrodriguezfc133@gmail.com>
2018-09-17 15:28:26 -0600

    agregando servicios de deparment, officeAsigment y studentGrade

---
+++ src/app/public/layout/Services/departament.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { RootService } from './root.service';
+
+@Injectable()
+export class DepartamentService {
+
+    constructor(private rootService: RootService) { }
+    ruta = 'departament/';
+    pagination: string;
+
+    getAll(pageable, page, size): Promise<any> {
+    this.pagination = '?pageable=' + pageable + '&page' + page + '&size' + size;
+    return this.rootService.getAll(this.ruta + this.pagination);
+    }
+
+    getOne(id: number): Promise<any> {
+        return this.rootService.getOne(this.ruta + id);
+    }
+
+    post(data: any): Promise<any> {
+        return this.rootService.post(this.ruta, data);
+    }
+
+    put(id: number, data: any): Promise<any> {
+        return this.rootService.put(this.ruta + id, data);
+    }
+
+    patch(id: number, data: any): Promise<any> {
+        return this.rootService.patch(this.ruta + id, data);
+    }
+
+    delete(id: number): Promise<any> {
+        return this.rootService.delete(this.ruta + id);
+    }
+}
